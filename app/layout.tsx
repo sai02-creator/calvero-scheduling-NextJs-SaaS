36d6f5cdbe1ee014f1be5a32ed829b393f3ad5d0
Gaddam Sai chaithanya <gaddamsaichaithanya@Gaddams-MacBook-Air.local>
2026-01-17 10:19:05 +1100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+import {
+  ClerkProvider,
+ 
+} from '@clerk/nextjs'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Calvero Scheduling SaaS',
+  description: 'Calvero Scheduling SaaS',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
